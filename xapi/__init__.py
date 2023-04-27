423008de9e801eac6a5d880519023c4100fba115
Paweł Knioła <pawel.kn@gmail.com>
2023-04-27 08:12:45 +0200

    Fix module imports

--- xapi/__init__.py
+++ xapi/__init__.py
@@ -7,6 +7,7 @@ __version__ = "0.0.1"
 
 from .xapi import XAPI, connect
 from .enums import TradeCmd, TradeType, TradeStatus, PeriodCode
+from .connection import Connection
 from .socket import Socket
 from .stream import Stream
 from .exceptions import ConnectionClosed, LoginFailed
