811dd9b565d7a29489e525efb562a46f42347cf0
Paweł Knioła <pawel.kn@gmail.com>
2023-04-28 10:38:15 +0200

    Update project build files

--- setup.py
+++ setup.py
@@ -20,6 +20,6 @@ setup(
         "Programming Language :: Python :: 3.12"
     ],
     python_requires='>=3.7',
-    version="0.0.2",
+    version="0.0.3",
     packages=['xapi'],
 )
--- xapi/__init__.py
+++ xapi/__init__.py
@@ -3,7 +3,7 @@
 """
 
 name = "xapi"
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 from .xapi import XAPI, connect
 from .enums import TradeCmd, TradeType, TradeStatus, PeriodCode
