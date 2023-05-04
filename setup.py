a36a1033689f92075091bdb7b2ba5e0ad170ba8d
Paweł Knioła <pawel.kn@gmail.com>
2023-05-04 12:11:58 +0200

    PyProject: Fix requirements

--- setup.py
+++ setup.py
@@ -20,6 +20,6 @@ setup(
         "Programming Language :: Python :: 3.12"
     ],
     python_requires='>=3.7',
-    version="0.1.2",
+    version="0.1.3",
     packages=['xapi'],
 )
--- xapi/__init__.py
+++ xapi/__init__.py
@@ -3,7 +3,7 @@
 """
 
 name = "xapi"
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 from .xapi import XAPI, connect
 from .enums import TradeCmd, TradeType, TradeStatus, PeriodCode
